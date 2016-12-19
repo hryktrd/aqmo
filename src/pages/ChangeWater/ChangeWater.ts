3f63dfc68c58ee3ba914e439ba22bcb9885765b5
Hiroyuki Terada <hissan1983@gmail.com>
2016-12-19 15:00:54 +0900

    水換え日の新規追加

--- src/pages/ChangeWater/ChangeWater.ts
+++ src/pages/ChangeWater/ChangeWater.ts
@@ -1,29 +1,44 @@
 import {Component, OnInit} from '@angular/core';
-
-import { NavController } from 'ionic-angular';
-
-import {ChangeWaterDate} from "../../app/ChangeWaterDate";
+import {NavController} from 'ionic-angular';
 import {ChangeWaterDateService} from "../../services/ChangeWaterDateService";
+import * as moment from 'moment'
 
 @Component({
-  selector: 'change-water',
-  templateUrl: 'ChangeWater.html',
-  providers: [ChangeWaterDateService]
+    selector: 'change-water',
+    templateUrl: 'ChangeWater.html',
+    providers: [ChangeWaterDateService]
 })
 
-export class ChangeWater implements OnInit{
+export class ChangeWater implements OnInit {
+    private todayDate = moment().format('YYYY-MM-DD');
+    private newChangeWater = {
+        "date": this.todayDate
+    };
 
-  constructor(public navCtrl: NavController, private changeWaterDateService: ChangeWaterDateService) {
+    constructor(public navCtrl: NavController, private changeWaterDateService: ChangeWaterDateService) {
 
-  }
-  private changeWaterDates: ChangeWaterDate[];
-  public event = {
-    month: '1990-02-19',
-    timeStarts: '07:43',
-    timeEnds: '1990-02-20'
-  }
-  ngOnInit() {
-    this.changeWaterDateService.getChangeWaterDates().then(changeWaterDates => this.changeWaterDates = changeWaterDates).then(()=>console.log(this.changeWaterDates.length));
-  }
+    }
 
+    private changeWaterDates = [
+        {
+            "date": '2016-02-19',
+        },
+        {
+            "date": '2016-03-19',
+        },
+        {
+            "date": '2016-04-19',
+        },
+    ];
+
+    ngOnInit(){
+        // this.changeWaterDateService.getChangeWaterDates().then(changeWaterDates => this.changeWaterDates = changeWaterDates).then(() => console.log(this.changeWaterDates.length));
+    }
+
+    addChangeWaterDate(){
+        this.changeWaterDates.push({
+            "date": this.newChangeWater.date
+        });
+        console.log(this.changeWaterDates);
+    }
 }
