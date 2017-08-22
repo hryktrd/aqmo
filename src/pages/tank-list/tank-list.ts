85fdf0fb5805ed27947371739d0a0721641fe9a9
Hiroyuki Terada <hissan1983@gmail.com>
2017-08-22 16:52:11 +0900

    水槽一覧ページ作成

--- src/pages/tank-list/tank-list.ts
+++ src/pages/tank-list/tank-list.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {Tank} from "../../dto/Tank";
+import {TankListService} from "../../services/tank-list.service";
 
 /**
  * Generated class for the TankListPage page.
@@ -9,16 +11,20 @@ import { NavController, NavParams } from 'ionic-angular';
  */
 
 @Component({
-  selector: 'page-tank-list',
-  templateUrl: 'tank-list.html',
+    selector: 'page-tank-list',
+    templateUrl: 'tank-list.html',
 })
 export class TankListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    tank: Tank;
+    tanks: Tank[];
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TankListPage');
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, private tankListService: TankListService) {
+        this.tanks = tankListService.tanks;
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad TankListPage');
+    }
 
 }
